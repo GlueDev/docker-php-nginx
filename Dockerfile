08d186e31a9d1ba40e0480239d4e35e8d743fd0d
Diederik van den Burger <diederik@glue.group>
2018-11-23 13:25:38 +0100

    chore: initial commit

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM webdevops/php-nginx:7.2
+
+# PHP settings
+ENV PHP_DATE_TIMEZONE Europe/Amsterdam
+
+# Glue ENV variables
+ENV ENABLE_BATCH_DAEMON false
+ENV ENABLE_COMPOSER true
+ENV ENABLE_LARAVEL_HORIZON false
+ENV ENABLE_LARAVEL_NGINX true
+
+# Glue config & entrypoint
+COPY ./config /opt/glue/
+COPY ./entrypoint.sh /entrypoint.d/glue.sh
+
+EXPOSE 80
---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+ #!/bin/bash
+
+if [[ "$ENABLE_BATCH_DAEMON" = true ]]; then
+    cp /opt/glue/batch-daemon.conf /opt/docker/etc/supervisor.d/batch-daemon.conf
+fi
+
+if [[ "$ENABLE_LARAVEL_NGINX" = true ]]; then
+    cp /opt/glue/laravel-nginx.conf /opt/docker/etc/nginx/vhost.conf
+fi
+
+if [[ "$ENABLE_LARAVEL_HORIZON" = true ]]; then
+    cp /opt/glue/laravel-horizon.conf /opt/docker/etc/supervisor.d/laravel-horizon.conf
+fi
+
+if [[ "$ENABLE_COMPOSER" = true ]]; then
+    su - application -c "cd $WEB_DOCUMENT_ROOT && composer install"
+fi
